86750cb66a5177925617319e808ffaac046d9299
TheConsoleLog <seidermann.paul@gmail.com>
2021-03-05 08:42:16 +0100

    update am 05.03.2021 um 08:42

--- src/main/java/com/theconsolelog/manager/Manager.java
+++ src/main/java/com/theconsolelog/manager/Manager.java
@@ -4,8 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Manager extends JavaPlugin {
 
+	private static Manager manager;
+
+	public static Manager getManager () {
+		return manager;
+	}
+
 	@Override
 	public void onEnable () {
+		manager = this;
 	}
 
 	@Override
